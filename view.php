d9d1f2bb4dec426d10dfa5fe80ae0deb0e4ccda3
sheinhtet715 <sheinhtet715@gmail.com>
2025-09-01 22:18:37 +0630

    table boostrap remove

--- view.php
+++ view.php
@@ -60,13 +60,30 @@ if(isset($_POST['update_button'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=s, initial-scale=1.0">
     <title>Document</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
      <style>
         body{
             padding : 25px;
 
         }
+
+        table {
+        width: 100%;
+        border-collapse: collapse; 
+        margin-bottom: 20px;
+        }
+
+        table, th, td {
+          border: 1px solid #b4b2b2ff; 
+        }
+
+        th, td {
+          padding: 8px;
+          text-align: left;
+        }
+        th {
+            background-color:  #d8dadcff;
+        }
+
     </style>
 </head>
 <body>
