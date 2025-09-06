f50af43e73fba8c059bc3afd8ea885b239a233b0
sheinhtet715 <sheinhtet715@gmail.com>
2025-09-06 15:12:44 +0630

    bootstrap link

--- view.php
+++ view.php
@@ -70,6 +70,7 @@ if(isset($_POST['update_button'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=s, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
      <style>
         *{
             margin: 0;
