b558238eab578457ce3de0b13df6a933ac4c873c
Marina Aye Thidar Tun <marinaayethidartun@gmail.com>
2025-09-06 15:40:11 +0630

    Padding

--- process.php
+++ process.php
@@ -13,6 +13,18 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+<style>
+        *{
+            margin: 0;
+            padding: 0;
+          
+        }
+        body{
+            padding: 20px 10px;
+
+
+        }
+</style>
 <body>
          <?php if(!isset($_SESSION['message'])): ?>
         <h1>New Record Created Successfully</h1>
